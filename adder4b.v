1e9a3ce5bb8cf084e6dbe94f2f340c48acbe5a2a
mts0629 <soratobi93@gmail.com>
2026-06-20 23:58:18 +0900

    4-bit adder

---
+++ adder4b.v
@@ -0,0 +1,11 @@
+// 4-bit adder
+module adder4b(in1, in2, out, cy);
+  input  [3:0] in1, in2; // Inputs
+  output [3:0] out;      // Output
+  output       cy;       // Carry
+
+  wire [4:0] res;
+  assign res = in1 + in2;
+  assign cy  = res[4];
+  assign out = res[3:0];
+endmodule
