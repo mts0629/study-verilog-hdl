04a8edb94ce817dc5843b18c8eee79791f10ddf8
mts0629 <soratobi93@gmail.com>
2026-06-23 23:57:18 +0900

    4-bit multiplexer

---
+++ mux4_4b/mux4_4b.v
@@ -0,0 +1,13 @@
+// 4-bit multiplexer
+module mux4_4b(sel, in0, in1, in2, in3, out);
+  input  [1:0] sel;
+  input  [3:0] in0, in1, in2, in3;
+  output [3:0] out;
+
+  assign out = (sel == 0) ? in0 :
+               (sel == 1) ? in1 :
+               (sel == 2) ? in2 :
+               (sel == 3) ? in3 :
+               4'bx; // Unknown
+endmodule
+
---
+++ mux4_4b/testbench.v
@@ -0,0 +1,27 @@
+`timescale 1 ns/100 ps
+
+module mux4_4b_tb();
+  reg  [1:0] sel;
+  reg  [3:0] in0, in1, in2, in3;
+  wire [3:0] result;
+
+  mux4_4b mux(sel, in0, in1, in2, in3, result);
+
+  initial begin
+    sel <= 2'b00;
+    in0 <= 4'b0001;
+    in1 <= 4'b0010;
+    in2 <= 4'b0100;
+    in3 <= 4'b1000;
+
+    #1 sel <= 2'b01;
+    #1 sel <= 2'b10;
+    #1 sel <= 2'b11;
+    #1 $finish;
+  end
+
+  initial begin
+    $dumpfile("mux4_4b.vcd");
+    $dumpvars(0, mux);
+  end
+endmodule
