ccecb345bba799e37c68e2a556d91e9d43a0a381
mts0629 <soratobi93@gmail.com>
2026-06-28 00:23:18 +0900

    Control left/right shift

--- barrel_shifter/barrel_shifter.v
+++ barrel_shifter/barrel_shifter.v
@@ -1,8 +1,10 @@
 // 8-bit barrel shifter
-module barrel_shifter(in, sft, out);
+module barrel_shifter(in, sft, lr, out);
     input  [7:0] in;
     input  [2:0] sft;
+    input        lr;  // 1=left, 0=right
     output [7:0] out;
 
-    assign out = (in << sft | in >> (8 - sft));
+    assign out = lr ? (in << sft | in >> (8 - sft))
+                    : (in >> sft | in << (8 - sft));
 endmodule
--- barrel_shifter/testbench.v
+++ barrel_shifter/testbench.v
@@ -3,16 +3,23 @@
 module barrel_shifter_tb();
     reg  [7:0] in;
     reg  [2:0] sft;
+    reg        lr;
     wire [7:0] out;
 
-    barrel_shifter shifter(in, sft, out);
+    barrel_shifter shifter(in, sft, lr, out);
+
+    integer i;
 
     initial begin
-        in  <= 8'b01000001;
-        sft <= 3'b000;
+        for (i = 0; i < 2; i = i + 1) begin
+            in  <= 8'b01000001;
+            sft <= 3'b000;
+            lr  <= i;
 
-        repeat(8)
-          #1 sft <= sft + 3'b001;
+            repeat(8) begin
+                #1 sft <= sft + 3'b001;
+            end
+        end
 
         #1 $finish;
     end
