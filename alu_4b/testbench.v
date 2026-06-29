ed054efc035536c72c446e89294d25016c4028ed
mts0629 <soratobi93@gmail.com>
2026-06-29 23:59:13 +0900

    Revise test bench

--- alu_4b/testbench.v
+++ alu_4b/testbench.v
@@ -9,15 +9,42 @@ module alu_4b_tb();
     alu_4b alu(a, b, op, res);
 
     initial begin
-        a  <= 4'b0110;
-        b  <= 4'b0001;
+        a  <= 4'b0111; // h7
+        b  <= 4'b0001; // h1
+
+        // Add: h7 + h1 = h8
         op <= 4'b0000;
+        #1;
+        // Sub: h7 - h1 = h6
+        op <= 4'b0001;
+        #1;
+        // And: h7 & h1 = h1 
+        op <= 4'b0010;
+        #1;
+        // Or: h7 | h1 = h7
+        op <= 4'b0011;
+        #1;
+        // Xor: h7 ^ h1 = h6
+        op <= 4'b0100;
+        #1;
+        // Shift left: h7 << h1 = he
+        op <= 4'b0101;
+        #1;
+        // Shift right: h7 >> h1 = h3
+        op <= 4'b0110;
+        #1;
+        // Arithmetic left shift: -h8 >> h1 = -h4 (4'b1100 = hc)
+        a  <= 4'b1000;
+        op <= 4'b0111;
+        #1;
+        // Less than: -h8 < h1 -> h1
+        op <= 4'b1000;
+        #1;
+        // Less than (unsigned): h8 > h1 -> h0
+        op <= 4'b1001;
+        #1;
 
-        repeat(10) begin
-            #1 op <= op + 1;
-        end
-
-        #1 $finish;
+        $finish;
     end
 
     initial begin
