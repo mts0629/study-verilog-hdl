c80ae37d3526aadf4f109430d32b2ad9d3a9f4c4
mts0629 <soratobi93@gmail.com>
2026-07-06 23:46:11 +0900

    Add asynchronous reset

--- register_4b/reg_4b.v
+++ register_4b/reg_4b.v
@@ -1,12 +1,17 @@
 // 4-bit register
-module reg_4b(stb, d, q);
-    input        stb; // Strobe signal
-    input  [3:0] d;   // Input
-    output [3:0] q;   // Output
+module reg_4b(stb, rst_n, d, q);
+    input        stb;   // Strobe signal
+    input        rst_n; // Asynchronous reset signal
+    input  [3:0] d;     // Input
+    output [3:0] q;     // Output
 
     reg    [3:0] q;
 
-    always @(posedge stb) begin
-        q <= d;
+    always @(posedge stb or negedge rst_n) begin
+        if (!rst_n) begin
+            q <= 4'h0;
+        end else begin
+            q <= d;
+        end
     end
 endmodule
--- register_4b/reg_4b_tb.v
+++ register_4b/reg_4b_tb.v
@@ -2,14 +2,16 @@
 
 module reg_4b_tb();
     reg        stb;
+    reg        rst_n; 
     reg  [3:0] d;
     wire [3:0] q;
 
-    reg_4b r4b(stb, d, q);
+    reg_4b r4b(stb, rst_n, d, q);
 
     initial begin
-        stb <= 1;
-        d  <= 1'h0;
+        stb   <= 1;
+        rst_n <= 1;
+        d     <= 1'h0;
 
         #1 stb <= 0;
         #1 d <= 4'h5;
@@ -19,8 +21,7 @@ module reg_4b_tb();
         #1 stb <= 1;
         #1 d <= 4'h1;
         #1 stb <= 0;
-        #1 stb <= 1;
-        #1 stb <= 0;
+        #1 rst_n <= 0;
 
         #1 $finish;
     end
