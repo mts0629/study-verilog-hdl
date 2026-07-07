1cf0dcec0fcfc85bbd6cf9afa844272db55659f1
mts0629 <soratobi93@gmail.com>
2026-07-07 23:59:17 +0900

    Add write enable signal

--- register_4b/reg_4b.v
+++ register_4b/reg_4b.v
@@ -1,6 +1,7 @@
 // 4-bit register
-module reg_4b(stb, rst_n, d, q);
+module reg_4b(stb, en, rst_n, d, q);
     input        stb;   // Strobe signal
+    input        en;    // Write enable signal
     input        rst_n; // Asynchronous reset signal
     input  [3:0] d;     // Input
     output [3:0] q;     // Output
@@ -10,8 +11,9 @@ module reg_4b(stb, rst_n, d, q);
     always @(posedge stb or negedge rst_n) begin
         if (!rst_n) begin
             q <= 4'h0;
-        end else begin
+        end else if (en) begin
             q <= d;
         end
+        // Otherwise, hold data
     end
 endmodule
--- register_4b/reg_4b_tb.v
+++ register_4b/reg_4b_tb.v
@@ -2,26 +2,35 @@
 
 module reg_4b_tb();
     reg        stb;
+    reg        en;
     reg        rst_n; 
     reg  [3:0] d;
     wire [3:0] q;
 
-    reg_4b r4b(stb, rst_n, d, q);
+    reg_4b r4b(stb, en, rst_n, d, q);
 
     initial begin
-        stb   <= 1;
-        rst_n <= 1;
+        stb   <= 0;
+        en    <= 0;
+        rst_n <= 0;       // Reset
         d     <= 1'h0;
 
-        #1 stb <= 0;
-        #1 d <= 4'h5;
-        #1 stb <= 1;
-        #1 stb <= 0;
-        #1 d <= 4'h3;
-        #1 stb <= 1;
-        #1 d <= 4'h1;
-        #1 stb <= 0;
-        #1 rst_n <= 0;
+        #1 rst_n <= 1;
+        #1 d     <= 4'h1;
+        #1 stb   <= 1;    // q doesn't be changed
+        #1 stb   <= 0;
+        // q=4'h3
+        #1 en    <= 1;    // Write enable
+        #1 d     <= 4'h3;
+        #1 stb   <= 1;    // q=4'h3
+        #1 stb   <= 0;
+        // En=1
+        #1 d     <= 4'h5;
+        #1 stb   <= 1;    // q=4'h5
+        #1 stb   <= 0;
+        // Reset
+        #1 d     <= 4'h1;
+        #1 rst_n <= 0;    // Reset, q=4'h0
 
         #1 $finish;
     end
