df4d7d36d7b781b1a212676cdc1cf09cc50cece0
mts0629 <soratobi93@gmail.com>
2026-07-23 23:57:52 +0900

    Visualize by yosys

--- synth.tcl
+++ synth.tcl
@@ -6,6 +6,8 @@ yosys hierarchy -check -top $::env(TOP_MODULE)
 
 # The high-level stuff
 yosys proc; yosys opt
+yosys fsm; yosys opt
+yosys memory; yosys opt
 
 # Mapping to internal cell library
 yosys techmap; yosys opt
@@ -15,3 +17,6 @@ yosys clean
 
 # Write synthesized design
 yosys write_verilog synth.v
+
+# Visualize the design (in simple RTL netlist)
+yosys show
